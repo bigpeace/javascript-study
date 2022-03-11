284837917d44d91d1c7387a5ce9c86330890e902
Yamato-Suzuki <bigpiece2013.dream@gmail.com>
2022-03-12 08:10:45 +0900

    step1-2

---
+++ step1/main.js
@@ -0,0 +1,13 @@
+// alert('test');
+var displayElm = document.getElementsByClassName('display')[0];
+var startButton = document.getElementsByClassName('startButton')[0];
+
+startButton.addEventListener('click', function(){
+    console.log('start');
+    var seconds = 0;
+    setInterval(function(){
+        seconds++;
+        displayElm.innerText = seconds;
+        console.log(seconds);
+    }, 1000);
+});
