1d5afa40af95b6078d934db5fd8ccaa9738a179c
Yamato-Suzuki <bigpiece2013.dream@gmail.com>
2022-03-14 05:06:10 +0900

    Timer 初期セット

--- step1/main.js
+++ step1/main.js
@@ -1,13 +1,21 @@
 // alert('test');
 var displayElm = document.getElementsByClassName('display')[0];
-var startButton = document.getElementsByClassName('startButton')[0];
+var timer = null;
 
+var startButton = document.getElementsByClassName('startButton')[0];
 startButton.addEventListener('click', function(){
-    console.log('start');
     var seconds = 0;
-    setInterval(function(){
+    timer = setInterval(function(){
         seconds++;
         displayElm.innerText = seconds;
         console.log(seconds);
     }, 1000);
+    console.log('start:' + timer)
 });
+
+var stopButton = document.getElementsByClassName('stopButton')[0];
+stopButton.addEventListener('click', function(){
+    console.log('stop:' + timer);
+    clearInterval(timer);
+    timer = null;
+})
