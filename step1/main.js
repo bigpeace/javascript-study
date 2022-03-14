84f5a5b278643665e0c8baf4524252e5a16461e0
Yamato-Suzuki <bigpiece2013.dream@gmail.com>
2022-03-15 05:10:27 +0900

    modify timer start stop

--- step1/main.js
+++ step1/main.js
@@ -4,18 +4,22 @@ var timer = null;
 
 var startButton = document.getElementsByClassName('startButton')[0];
 startButton.addEventListener('click', function(){
-    var seconds = 0;
-    timer = setInterval(function(){
-        seconds++;
-        displayElm.innerText = seconds;
-        console.log(seconds);
-    }, 1000);
-    console.log('start:' + timer)
+    if (timer === null) {
+        var seconds = 0;
+        timer = setInterval(function(){
+            seconds++;
+            displayElm.innerText = seconds;
+            console.log(seconds);
+        }, 1000);
+        console.log('start:' + timer)
+    }
 });
 
 var stopButton = document.getElementsByClassName('stopButton')[0];
 stopButton.addEventListener('click', function(){
-    console.log('stop:' + timer);
-    clearInterval(timer);
-    timer = null;
+    if (timer !== null) {
+        console.log('stop:' + timer);
+        clearInterval(timer);
+        timer = null;
+    }
 })
